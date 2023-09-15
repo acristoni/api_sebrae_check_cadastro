2c44c02c1237153ca7b5e6c92699ec4c7f27e8f8
acristoni <adonai_cn@hotmail.com>
2023-09-15 10:58:41 -0300

    BUILD: Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Microsserviço-SEBRAE')
+    .setDescription(
+      'Microsserviço verificação de associação do usuário GroodMe a entidade SEBRAE',
+    )
+    .setVersion('1.0')
+    .addTag('Verificação')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const PORT = process.env.PORT || 3333;
   await app.listen(PORT);
 }
