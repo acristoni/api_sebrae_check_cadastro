03b706a194057ae392d5387f9529d7b5d24d00f9
acristoni <adonai_cn@hotmail.com>
2023-09-15 11:04:13 -0300

    FEAT: controller validacao

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Verificação')
 @Controller()
@@ -8,7 +8,19 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Apenas para teste de funcionamento do microsserviço',
+  })
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('validacao/:cpf')
+  @ApiOperation({
+    summary:
+      'Verifica se pessoa possui vínculo com o SEBRAE e persiste em BD próprio.',
+  })
+  async validacao(@Param('cpf') cpf: string) {
+    return await this.appService.validacao(cpf);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Microsserviço em execução!';
   }
+
+  async validacao(cpf: string) {
+    return cpf;
+  }
 }
